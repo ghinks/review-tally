46191196a142ece068c604f66492225341841795
ghinks <ghinks@yahoo.com>
2024-11-19 16:55:08 -0500

    feat: reviews by reviewer
    - remove main section from module

--- queries/get_reviewers_rest.py
+++ queries/get_reviewers_rest.py
@@ -14,11 +14,3 @@ def get_reviewers_for_pull_request(owner: str, repo: str, pull_number: int) -> l
     reviews = response.json()
     reviewers = [review['user'] for review in reviews]
     return reviewers
-
-# if __name__ == '__main__':
-#     owner_name = "owner_name"
-#     repo_name = "repo_name"
-#     pull_number = 1  # Replace with the actual pull request number
-#     reviewers = get_reviewers_for_pull_request(owner_name, repo_name, pull_number)
-#     for reviewer in reviewers:
-#         print(f"Reviewer: {reviewer['login']}, URL: {reviewer['html_url']}")
