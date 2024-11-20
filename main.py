a56dd03b52deb091b2ac3b415defed4fabdef1fa
ghinks <ghinks@yahoo.com>
2024-11-20 08:30:20 -0500

    chore: remove unused dependencies
    - remove commented out prints
    - use cli args for dates

--- main.py
+++ main.py
@@ -11,19 +11,14 @@ if __name__ == "__main__":
     org_name, start_date, end_date = parse_cmd_line()
     repositories = get_org_repos(org_name)
     for repo in repositories:
-        # print(f"Name: {repo['name']}, URL: {repo['html_url']}")
-        start_date = datetime.strptime("2024-11-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
-        end_date = datetime.strptime("2024-12-01T23:59:59Z", "%Y-%m-%dT%H:%M:%SZ")
         pull_requests = get_pull_requests_between_dates(
             org_name, repo["name"], start_date, end_date
         )
         for pr in pull_requests:
-            # print(f"ID: {pr['id']}, Title: {pr['title']}, Created At: {pr['created_at']}, URL: {pr['html_url']}")
             reviewers = get_reviewers_for_pull_request(
                 org_name, repo["name"], pr["number"]
             )
             for reviewer in reviewers:
-                # print(f"Reviewer: {reviewer['login']}, URL: {reviewer['html_url']}")
                 if reviewer["login"] in reviewer_prs:
                     reviewer_prs[reviewer["login"]] += 1
                 else:
