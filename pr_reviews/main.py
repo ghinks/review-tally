14bd1bf93fbd6daae358aaa8dab3a8da3ed95a80
ghinks <ghinks@yahoo.com>
2024-12-03 11:15:22 -0500

    feat: alter imports for packaging
    - re-factoring

--- pr_reviews/main.py
+++ pr_reviews/main.py
@@ -1,12 +1,13 @@
-from queries.get_prs import get_pull_requests_between_dates
+from .queries.get_prs import get_pull_requests_between_dates
 
 # from queries.get_repos_rest import get_org_repos
-from queries.get_repos_gql import get_repos_by_language
-from queries.get_reviewers_rest import get_reviewers_for_pull_request
+from .queries.get_repos_gql import get_repos_by_language
+from .queries.get_reviewers_rest import get_reviewers_for_pull_request
 from tabulate import tabulate
-from cli.parse_cmd_line import parse_cmd_line
+from .cli.parse_cmd_line import parse_cmd_line
 
-if __name__ == "__main__":
+
+def main():
     # map containing the reviewer name and the number of pull requests reviewed
     reviewer_prs = {}
     org_name, start_date, end_date, language = parse_cmd_line()
@@ -30,3 +31,7 @@ if __name__ == "__main__":
     # convert the dictionary to a list of lists and sort by the number of PRs reviewed
     table = sorted(table, key=lambda x: x[1], reverse=True)
     print(tabulate(table))
+
+
+if __name__ == "__main__":
+    main()
