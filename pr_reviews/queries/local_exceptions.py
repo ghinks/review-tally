4609b2343979b4546e6dcd7d44af2d7e0cc9ed3f
ghinks <ghinks@yahoo.com>
2024-12-21 13:35:19 -0500

    feat: ruff
    - lint main.py
    - ignore other files for now

--- pr_reviews/queries/local_exceptions.py
+++ pr_reviews/queries/local_exceptions.py
@@ -2,3 +2,10 @@ class GitHubTokenNotDefinedError(Exception):
     """Exception raised when the GitHub token is not defined."""
 
     pass
+
+
+class LoginNotFoundError(ValueError):
+    """Exception raised when the login is not found in the reviewer."""
+
+    def __init__(self):
+        super().__init__("login not found in reviewer")
