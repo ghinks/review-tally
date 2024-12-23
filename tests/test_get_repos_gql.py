b19073da83804997af625b3d58e8a6885369bcde
ghinks <ghinks@yahoo.com>
2024-12-23 16:11:22 -0500

    bug fix gh token

--- tests/test_get_repos_gql.py
+++ tests/test_get_repos_gql.py
@@ -1,11 +1,12 @@
 import unittest
 from unittest.mock import Mock, patch
-
+import os
 from pr_reviews.queries.get_repos_gql import get_repos_by_language
 
 
 class TestGetReposByLanguage(unittest.TestCase):
     @patch("pr_reviews.queries.get_repos_gql.requests.post")
+    @patch.dict(os.environ, {"GITHUB_TOKEN": "test_token"})
     def test_get_repos_by_language(self, mock_post) -> None:  # noqa: ANN001
         # Mock the response from the GitHub API
         mock_response = Mock()
